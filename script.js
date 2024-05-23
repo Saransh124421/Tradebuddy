f552cc66efa3ba0431d098c9bfe6ff4da2ae01aa
Saransh124421 <121530890+Saransh124421@users.noreply.github.com>
2024-05-23 19:37:45 +0530

    initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('gapup').addEventListener('click', function() {
+    window.location.href = 'https://tradebuddy-6.onrender.com/api/stocks/gap_up';
+});
+
+document.getElementById('gapdown').addEventListener('click', function() {
+    window.location.href = 'https://tradebuddy-6.onrender.com/api/stocks/gap_down';
+});
