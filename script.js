7c1c886ac67c74351533b0609d2d0dd29d37c998
Saransh124421 <121530890+Saransh124421@users.noreply.github.com>
2024-05-31 22:01:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('gapup').addEventListener('click', function() {
-    window.location.href = 'https://tradebuddy-6.onrender.com/api/stocks/gap_up';
-});
-
-document.getElementById('gapdown').addEventListener('click', function() {
-    window.location.href = 'https://tradebuddy-6.onrender.com/api/stocks/gap_down';
-});
